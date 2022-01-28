704be2c6d6de207afeb806cbf4c17c60421c2a82
Peter Oehman <tucker2923@gmail.com>
2022-01-28 18:57:31 -0500

    using fields to save specific parts of an updated model, and using reload

--- db.js
+++ db.js
@@ -17,19 +17,37 @@ User.prototype.respond = function() {
   return this.firstName + ' ' + this.lastName
 }
 
-async function create() {
-  const user = await User.create({ firstName: 'Peter', lastName: 'Oehman' })
-  console.log(user.respond())
-  console.log(user.toJSON())
+User.talk = function() {
+  console.log('Im a class method!')
 }
 
-create()
-
+async function create() {
+  try {
+    User.talk()
+    const user = await User.create({ firstName: 'Peter', lastName: 'Oehman' })
+    console.log(user.toJSON())
+    user.set({
+      firstName: "John",
+      lastName: "Duffy"
+    })
+    await user.save()
+    console.log(user.respond())
+    const user2 = await User.create({ firstName: 'Bob', lastName: 'Bobby' })
+    user2.firstName = 'jack'
+    user2.lastName = 'jacky'
+    await user2.save({ fields: ['firstName'] })
+    console.log(user2.toJSON())
+    await user2.reload()
+    console.log(user2.toJSON())
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 async function connect() {
   try {
-    await db.sync({ alter: true })
-    console.log('worked')
+    await db.sync({ force: true })
+    await create()
     await db.close()
   } catch (error) {
     console.log('didnt work')
