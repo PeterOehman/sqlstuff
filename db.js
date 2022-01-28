5dff014d6ca4e5dd38eeb492884f087d3ccf6684
Peter Oehman <tucker2923@gmail.com>
2022-01-28 13:35:45 -0500

    added basic user model

--- db.js
+++ db.js
@@ -1,9 +1,22 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost:5432/sqlstuff')
+const db = new Sequelize('postgres://localhost:5432/sqlstuff', {
+  logging: false
+})
+
+const User = db.define('User', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }}, {
+    timestamps: false
+})
 
 async function connect() {
   try {
-    await db.authenticate()
+    await db.sync({ alter: true })
     console.log('worked')
     await db.close()
   } catch (error) {
