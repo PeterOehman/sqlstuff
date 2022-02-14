1ea4a03f2d779d0db724cc1f713b5f356da66aab
Peter Oehman <tucker2923@gmail.com>
2022-02-14 12:47:54 -0500

    using include with an alias and using required

--- db_8.js
+++ db_8.js
@@ -19,14 +19,22 @@ const Tool = db.define('tool', {
 User.hasMany(Task)
 Task.belongsTo(User)
 User.hasMany(Tool, { as: 'Instruments' })
+// Tool.belongsTo(User)
 
 async function create() {
   try {
     const user = await User.create({ name: 'John Doe' })
     const task = await Task.create({ name: 'A Task' })
+    const tool = await Tool.create({ name: 'Scissor', size: 'small' })
     await user.addTask(task)
-    const tasks = await Task.findAll({ include: User })
+    await user.addInstrument(tool)
+    //required true will not return any tasks that dont have a user
+    const tasks = await Task.findAll({ include: { model: User, required: true }})
     console.log(JSON.stringify(tasks, null, 2))
+    // const users = await User.findAll({ include: Task })
+    // console.log(JSON.stringify(users, null, 2))
+    // const tools = await User.findAll({ include: 'Instruments' })
+    // console.log(JSON.stringify(tools, null, 2))
   } catch (error) {
     console.error(error)
   }
