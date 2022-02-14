c39494d86822b53aeeb90283c0267d2c222262c0
Peter Oehman <tucker2923@gmail.com>
2022-02-14 12:21:09 -0500

    using paranoid

--- db_7.js
+++ db_7.js
@@ -8,7 +8,7 @@ const Foo = db.define('foo', {
     type: Sequelize.STRING,
     unique: true
   }
-}, { timestamps: false })
+}, { paranoid: true })
 
 const Bar = db.define('bar', {
   name: {
@@ -22,10 +22,10 @@ Bar.belongsTo(Foo, { targetKey: 'name', foreignKey: 'fooName' })
 
 
 // methods with one to one
-const create = async function() {
-  const foo = await Foo.create({ name: 'the-foo' });
-  const bar1 = await Bar.create({ name: 'some-bar', fooName: 'the-foo' });
-  const bar2 = await Bar.create({ name: 'another-bar' });
+// const create = async function() {
+//   const foo = await Foo.create({ name: 'the-foo' });
+//   const bar1 = await Bar.create({ name: 'some-bar', fooName: 'the-foo' });
+//   const bar2 = await Bar.create({ name: 'another-bar' });
   // console.log(await foo.getBar()); // null
   // await foo.setBar(bar1);
   // console.log((await foo.getBar()).toJSON()); // 'some-bar'
@@ -35,30 +35,32 @@ const create = async function() {
   // console.log(newlyAssociatedBar.name); // 'yet-another-bar'
   // await foo.setBar(null); // Un-associate
   // console.log((await foo.getBar())); // null
-}
-
-//methods with one to many
-// const create = async function() {
-//   const foo = await Foo.create({ name: 'the-foo' })
-//   const bar1 = await Bar.create({ name: 'some-bar' })
-//   const bar2 = await Bar.create({ name: 'another-bar' })
-//   console.log(await foo.getBars())
-//   console.log(await foo.countBars())
-//   console.log(await foo.hasBar(bar1))
-//   await foo.addBars([bar1, bar2])
-//   console.log(await foo.countBars())
-//   const bars = (await foo.getBars())
-//   console.log(bars[0].toJSON(), bars[1].toJSON())
-//   await foo.addBar(bar1)
-//   console.log(await foo.countBars())
-//   console.log(await foo.hasBar(bar1))
-//   await foo.removeBar(bar2)
-//   console.log(await foo.countBars())
-//   await foo.createBar({ name: 'yet-another-bar' })
-//   console.log(await foo.countBars())
-//   await foo.setBars([])
-//   console.log(await foo.countBars())
 // }
+
+// methods with one to many
+const create = async function() {
+  const foo = await Foo.create({ name: 'the-foo' })
+  const bar1 = await Bar.create({ name: 'some-bar' })
+  const bar2 = await Bar.create({ name: 'another-bar' })
+  console.log(await foo.getBars())
+  console.log(await foo.countBars())
+  console.log(await foo.hasBar(bar1))
+  await foo.addBars([bar1, bar2])
+  console.log(await foo.countBars())
+  const bars = (await foo.getBars())
+  console.log(bars[0].toJSON(), bars[1].toJSON())
+  await foo.addBar(bar1)
+  console.log(await foo.countBars())
+  console.log(await foo.hasBar(bar1))
+  await foo.removeBar(bar2)
+  console.log(await foo.countBars())
+  await foo.createBar({ name: 'yet-another-bar' })
+  console.log(await foo.countBars())
+  // await foo.setBars([])
+  await foo.destroy( /* { force: true } */ )
+  console.log((await Foo.findByPk(1, { paranoid: false })).toJSON())
+  await foo.restore()
+}
 
 
 const connect = async function() {
